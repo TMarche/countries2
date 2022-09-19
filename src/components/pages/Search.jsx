7db4f5a94aba4b6d6d9502335e45c3413487b1ff
Troy Marchesseault <tmarchess@gmail.com>
2022-09-19 18:18:13 -0400

    Remove test data; add search filter functionality

--- src/components/pages/Search.jsx
+++ src/components/pages/Search.jsx
@@ -2,83 +2,45 @@ import styled from "styled-components";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const testCountries = [
-    {
-        flag: "https://flagcdn.com/w320/bg.png",
-        name: "Bulgaria",
-        population: 6927288,
-        region: "Europe",
-        capital: "Gaborone",
-        cioc: "BUL",
-    },
-    {
-        flag: "https://flagcdn.com/w320/bg.png",
-        name: "Bulgaria",
-        population: 6927288,
-        region: "Europe",
-        capital: "Gaborone",
-        cioc: "BUL",
-    },
-    {
-        flag: "https://flagcdn.com/w320/bg.png",
-        name: "Bulgaria",
-        population: 6927288,
-        region: "Europe",
-        capital: "Gaborone",
-        cioc: "BUL",
-    },
-    {
-        flag: "https://flagcdn.com/w320/bg.png",
-        name: "Bulgaria",
-        population: 6927288,
-        region: "Europe",
-        capital: "Gaborone",
-        cioc: "BUL",
-    },
-    {
-        flag: "https://flagcdn.com/w320/bg.png",
-        name: "Bulgaria",
-        population: 6927288,
-        region: "Europe",
-        capital: "Gaborone",
-        cioc: "BUL",
-    },
-    {
-        flag: "https://flagcdn.com/w320/bg.png",
-        name: "Bulgaria",
-        population: 6927288,
-        region: "Europe",
-        capital: "Gaborone",
-        cioc: "BUL",
-    },
-    {
-        flag: "https://flagcdn.com/w320/bg.png",
-        name: "Bulgaria",
-        population: 6927288,
-        region: "Europe",
-        capital: "Gaborone",
-        cioc: "BUL",
-    },
-    {
-        flag: "https://flagcdn.com/w320/bg.png",
-        name: "Bulgaria",
-        population: 6927288,
-        region: "Europe",
-        capital: "Gaborone",
-        cioc: "BUL",
-    },
-    {
-        flag: "https://flagcdn.com/w320/bg.png",
-        name: "Bulgaria",
-        population: 6927288,
-        region: "Europe",
-        capital: "Gaborone",
-        cioc: "BUL",
-    },
-];
-
 const Search = ({ className }) => {
-    let [countries, setCountries] = useState(testCountries);
+    let [countries, setCountries] = useState([]);
+    let [searchName, setSearchName] = useState("");
+    let [searchRegion, setSearchRegion] = useState("");
+
+    const renderCountries = (countries) => {
+        if (countries.length !== 0) {
+            return countries
+                .filter((country) =>
+                    country.name
+                        .toLowerCase()
+                        .includes(searchName.toLowerCase())
+                )
+                .filter(
+                    (country) =>
+                        searchRegion === "" || country.region === searchRegion
+                )
+                .map((country) => (
+                    <div className="countries--item">
+                        <img src={country.flag} alt={country.name + " flag"} />
+                        <div className="countries--item--content">
+                            <h2>{country.name}</h2>
+                            <div>
+                                <strong>Population: </strong>
+                                {country.population}
+                            </div>
+                            <div>
+                                <strong>Region:</strong> {country.region}
+                            </div>
+                            <div>
+                                <strong>Capital: </strong> {country.capital}
+                            </div>
+                        </div>
+                    </div>
+                ));
+        } else {
+            return <h2>Loading...</h2>;
+        }
+    };
 
     useEffect(() => {
         axios
@@ -114,41 +76,30 @@ const Search = ({ className }) => {
                     type="text"
                     id="search-by-name"
                     placeholder="Search for a country..."
+                    value={searchName}
+                    onChange={(e) => {
+                        setSearchName(e.target.value);
+                    }}
                 />
                 <input
                     type="select"
                     list="regions"
                     id="filter-by-region"
                     placeholder="Filter by Region"
+                    value={searchRegion}
+                    onChange={(e) => {
+                        setSearchRegion(e.target.value);
+                    }}
                 />
                 <datalist id="regions">
                     <option value="Africa">Africa</option>
-                    <option value="America">America</option>
+                    <option value="Americas">Americas</option>
                     <option value="Asia">Asia</option>
                     <option value="Europe">Europe</option>
                     <option value="Oceania">Oceania</option>
                 </datalist>
             </div>
-            <div className="countries">
-                {countries.map((country) => (
-                    <div className="countries--item">
-                        <img src={country.flag} alt={country.name + " flag"} />
-                        <div className="countries--item--content">
-                            <h2>{country.name}</h2>
-                            <div>
-                                <strong>Population: </strong>
-                                {country.population}
-                            </div>
-                            <div>
-                                <strong>Region:</strong> {country.region}
-                            </div>
-                            <div>
-                                <strong>Capital: </strong> {country.capital}
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </div>
+            <div className="countries">{renderCountries(countries)}</div>
         </div>
     );
 };
